a614d90e33e84e003ca884fe62a0f57adbb517a3
ali elamrani joutei <elamranijouteiali@gmail.com>
2019-11-11 18:03:56 +0100

    list of IPs and ARPs ready

--- IP ARP.py
+++ IP ARP.py
@@ -1,5 +1,15 @@
 from netmiko import ConnectHandler
+import re
 
+#IP address regex
+regex_ip = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
+            25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
+            25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
+            25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)'''
+
+# Mac address Regex
+regex_mac='([a-fA-F0-9]{2}[:|\-]?){6}'
+regex_cisco_mac='([a-fA-F0-9]{4}[:|\-.]?){3}'
 
 #Define SSH parameters
 ssh_port="22"
@@ -21,16 +31,32 @@ for ip in liste:
                         port = ssh_port,
                         username = username,
                         password = password) as ch:
-                                            
+                                          
     # Create a CLI command template
-        show_interface_config_temp = "show running-config interface {}"
+        #show_interface_config_temp = "show running-config interface {}"
 
     # Create desired CLI command and send to device
-        command = show_interface_config_temp.format("gi3")
-        interface = ch.send_command(command)
-        arp=ch.send_command("show ip arp")
-
+        #command = show_interface_config_temp.format("gi3")
+        #interface = ch.send_command(command)
+        arp_table=ch.send_command("show ip arp")
+        arp_liste=arp_table.split(" ")
+        print(arp_liste)
+        ip_and_mac_liste=[]
+	#search IPs and MAC and put them in a dictionnary 
+        for result in arp_liste:
+            #search for IP
+            ip=re.compile(regex_ip).search(result)
+            #search for mac
+            mac=re.compile(regex_mac).search(result)
+			#search for cisco mac
+            mac_cisco=re.compile(regex_cisco_mac).search(result)
+            if ip:
+                ip_and_mac_liste.append(result[ip.start():ip.end()])
+            if mac:
+                ip_and_mac_liste.append(result[mac.start():mac.end()])
+            if mac_cisco:
+                ip_and_mac_liste.append(result[mac_cisco.start():mac_cisco.end()])
+			
     # Print the raw command output to the screen
-        print(interface)				
-        print (arp)
+print (ip_and_mac_liste)
 				
