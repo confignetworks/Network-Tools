62276077b83b09c2deaaa34dd1fb5118e2491ad3
ali elamrani joutei <elamranijouteiali@gmail.com>
2019-11-12 17:47:55 +0100

    added interface where MAC is

--- get_mac_from_ip_function.py
+++ get_mac_from_ip_function.py
@@ -13,6 +13,9 @@ def get_mac_from_ip(wantedip):
 	regex_mac='([a-fA-F0-9]{2}[:|\-]?){6}'
 	regex_cisco_mac='([a-fA-F0-9]{4}[:|\-.]?){3}'
 
+	# Interface Regex
+	regex_int='^(TenGigabitEthernet|GigabitEthernet|Port-Channel|ethernet|Ethernet|Tunnel|Null|Vlan|Serial|Bundle-Ether)\d+[/]?[\d+]?[/]?[\d+]?[\.]?[\d+]?[/]?[\d+]?[/]?[\d+]?[:]?[\d+]?$'
+	
 	#Define SSH parameters
 	ssh_port="22"
 	username="admin"
@@ -22,7 +25,7 @@ def get_mac_from_ip(wantedip):
 	# Define IP and mac list     
 	ip_and_mac_list=[]
 
-	#Bring IP address from IP file 
+	#Bring device IP addresses from IP file 
 	with open('ip.txt','r') as ip:
 	   chaine=ip.read()
 	   device_list=chaine.split("\n")
@@ -46,10 +49,13 @@ def get_mac_from_ip(wantedip):
 				
 		# show ip arp                                    
 			arp_table=ch.send_command("show ip arp")
-		
+			
+		# Remove \n to keep interface
+			arp_table=arp_table.replace('\n',' ')
+			
 		# Create arp table list    
 			arp_list=arp_table.split(" ")
-			
+	
 		#search IPs and MAC and put them in a dictionnary 
 			for result in arp_list:
 		
@@ -61,13 +67,17 @@ def get_mac_from_ip(wantedip):
 		
 		#search for cisco mac
 				mac_cisco=re.compile(regex_cisco_mac).search(result)
-		
-		#Create ip and mac list
+	
+		#search for interface
+				int=re.compile(regex_int).search(result)
+		#Create ip, mac and int list
 				if ip:
 					ip_and_mac_list.append(result)
 				if mac:
 					ip_and_mac_list.append(result)
 				if mac_cisco:
+					ip_and_mac_list.append(result)
+				if int:
 					ip_and_mac_list.append(result)
         
 	# work and IP and MAC list to find the mac address of IP address
@@ -77,14 +87,17 @@ def get_mac_from_ip(wantedip):
 		# Find hostname
 		ip=re.compile(regex_ip).search(ip_in_list)
 		mac=re.compile(regex_cisco_mac).search(ip_in_list)
-		if not ip and not mac:
+		int=re.compile(regex_int).search(ip_in_list)
+		if not ip and not mac and not int:
 			hostname=ip_in_list
+		# if IP in the list
 		if ip_in_list==wantedip:
 			#print("mac address table for IP ", "is : ",ip_and_mac_list[position])
-			return hostname+"-"+ip_and_mac_list[position]
-
+			return hostname+"-"+ip_and_mac_list[position]+"-"+ip_and_mac_list[position+1]
+	# Script did not find the wanted IP"
+	return "0"
 if __name__ == "__main__":
 	#pop what is in args
 	for arg in sys.argv:
 		provided_ip=arg
-	print("mac address table for IP ",provided_ip," is : ",get_mac_from_ip(provided_ip))
+		print("mac address table for IP ",provided_ip," is : ",get_mac_from_ip(provided_ip))
