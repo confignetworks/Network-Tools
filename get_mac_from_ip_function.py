0625015f1fb95e85759ede33422cd25384c4cbba
ali elamrani joutei <elamranijouteiali@gmail.com>
2019-11-16 15:34:51 +0100

    solved timing issue

--- get_mac_from_ip_function.py
+++ get_mac_from_ip_function.py
@@ -1,8 +1,10 @@
 from netmiko import ConnectHandler
 import re
 import sys
+import time
 
 def get_mac_from_ip(wantedip):
+	
 	#IP address regex
 	regex_ip = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
 				25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
@@ -31,55 +33,60 @@ def get_mac_from_ip(wantedip):
 	   device_list=chaine.split("\n")
 		  
 	# Loop for all devices
-
+	
 	for device in device_list:
-				
+		
 		# Open CLI connection to device
 		with ConnectHandler(device,
 							device_type=device_ty,
 							port = ssh_port,
 							username = username,
 							password = password) as ch:
-		
+			
 		# Get device hostname
 			device_hostname=ch.send_command("sh run | s hostname")
-			hostname=device_hostname.split(" ")
-			hostname.remove("hostname")
-			ip_and_mac_list.append(hostname[0])
-				
-		# show ip arp                                    
+		# show ip arp
 			arp_table=ch.send_command("show ip arp")
+		
+		hostname=device_hostname.split(" ")
+		hostname.remove("hostname")
+		ip_and_mac_list.append(hostname[0])
+				
+                                    
 			
+		
+		
 		# Remove \n to keep interface
-			arp_table=arp_table.replace('\n',' ')
+		arp_table=arp_table.replace('\n',' ')
 			
 		# Create arp table list    
-			arp_list=arp_table.split(" ")
+		arp_list=arp_table.split(" ")
 	
 		#search IPs and MAC and put them in a dictionnary 
-			for result in arp_list:
 		
+		for result in arp_list:
+			
 		#search for IP
-				ip=re.compile(regex_ip).search(result)
+			ip=re.compile(regex_ip).search(result)
 		
 		#search for mac
-				mac=re.compile(regex_mac).search(result)
+			mac=re.compile(regex_mac).search(result)
 		
 		#search for cisco mac
-				mac_cisco=re.compile(regex_cisco_mac).search(result)
+			mac_cisco=re.compile(regex_cisco_mac).search(result)
 	
 		#search for interface
-				int=re.compile(regex_int).search(result)
+			int=re.compile(regex_int).search(result)
 		#Create ip, mac and int list
-				if ip:
-					ip_and_mac_list.append(result)
-				if mac:
-					ip_and_mac_list.append(result)
-				if mac_cisco:
-					ip_and_mac_list.append(result)
-				if int:
-					ip_and_mac_list.append(result)
-        
+			if ip:
+				ip_and_mac_list.append(result)
+			if mac:
+				ip_and_mac_list.append(result)
+			if mac_cisco:
+				ip_and_mac_list.append(result)
+			if int:
+				ip_and_mac_list.append(result)
+	
 	# work and IP and MAC list to find the mac address of IP address
 	position=0
 	for ip_in_list in ip_and_mac_list:
